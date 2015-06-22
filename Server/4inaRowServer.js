1e556bf3568b969c84e4c748f90ad3efac14dec5
grrd01 <gtyedmers@gmx.net>
2015-06-22 19:09:43 +0200

    merge from pages

---
+++ Server/4inaRowServer.js
@@ -0,0 +1,88 @@
+var app = require('http').createServer();
+var io = require('socket.io').listen(app);
+var fs = require('fs');
+var Moniker = require('moniker');
+var port = 3250;
+
+var users = [];
+var game = {
+		p1: null,
+		p2: null
+	};
+
+app.listen(port);
+
+// socket.io
+io.sockets.on('connection', function (socket) {
+	var user = addUser(socket);
+	//socket.emit("connect", user);
+
+	socket.on('disconnect', function () {
+		if (user.opponent != null) {
+			io.sockets.socket(user.opponent).emit("quit", user);
+		}
+		removeUser(user);
+	});
+
+	socket.on("playsend", function (data) {
+		io.sockets.socket(data.to).emit("playget", data);
+	});
+
+	socket.on("usersend", function (data) {
+		io.sockets.socket(data.to).emit("userget", data);
+	});
+
+
+});
+
+var addUser = function(socket) {
+	var user = {
+		name: Moniker.choose(),
+		id: socket.id,
+		role: null,
+		opponent: null
+	}
+	users.push(user);
+	updateUsers();
+	startgame(user);
+	return user;
+}
+
+var removeUser = function(user) {
+	for(var i=0; i<users.length; i++) {
+	if(user.name === users[i].name) {
+	users.splice(i, 1);
+	updateUsers();
+	return;
+	}
+	}
+}
+
+var updateUsers = function() {
+	var str = '';
+	for(var i=0; i<users.length; i++) {
+	var user = users[i];
+	str += user.name + ' <br />';
+	}
+	//io.sockets.emit("users", { users: str });
+}
+
+var startgame = function(user) {
+	for(var i=0; i<users.length; i++) {
+		if (i == users.length-1) {
+			// kein freier Gegner
+			io.sockets.socket(users[i].id).emit("connect", users[i]);
+		} else {
+			// Gegner vorhanden
+			if (users[i].opponent == null) {
+				users[i].opponent = users[users.length-1].id;
+				users[i].role = "0";
+				io.sockets.socket(users[i].id).emit("startgame", users[i]);
+				users[users.length-1].opponent = users[i].id;
+				users[users.length-1].role = "1";
+				io.sockets.socket(users[users.length-1].id).emit("startgame", users[users.length-1]);
+				break;
+			}
+		}
+	}
+}
---
+++ Server/start.bat
@@ -0,0 +1,2 @@
+cd "C:\Users\grrd\Documents\HTML5\4inaRow\Server"
+node 4inaRowServer.js
