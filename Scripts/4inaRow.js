d5ae067e55239e8f6bad91f1db3bc54be4cc5ad5
grrd01 <gtyedmers@gmx.net>
2016-02-22 19:54:19 +0100

    Server changed to openshift

--- Scripts/4inaRow.js
+++ Scripts/4inaRow.js
@@ -563,8 +563,8 @@
         $(".bt_online").addClass('ui-disabled');
         if (!connection) {
             //socket = io.connect('http://localhost:3250');
-            socket = io.connect('http://grrds4inarow.nodejitsu.com:80');
-            //socket = io.connect('http://4inarow-grrd.rhcloud.com:8000');
+            //socket = io.connect('http://grrds4inarow.nodejitsu.com:80');
+            socket = io.connect('http://4inarow-grrd.rhcloud.com:8000');
             connection = true;
         } else {
             socket.socket.reconnect();
