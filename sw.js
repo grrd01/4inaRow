eef6b0d921f3a0fa9967d03a00df3dd9a48e6c9f
(U109281) <gerard.tyedmers@mobi.ch>
2019-01-22 10:23:28 +0100

    serviceworker updated

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 /*jslint devel: true, browser: true */ /*global  self  */
 
 var CACHE_NAME = "grrds-4inarow-cache";
-var CACHE_VERSION = "v1.4";
+var CACHE_VERSION = "v1.5";
 var CACHE = CACHE_NAME + "-" + CACHE_VERSION;
 
 var urlsToCache = [
@@ -38,6 +38,7 @@ var urlsToCache = [
     "Images/red_on.png",
     "Images/settings.svg",
     "Images/stats.svg",
+    "Images/tictactoe.svg",
     "Images/title1.png",
     "Images/title2eng.png",
     "Scripts/4inaRow.css",
