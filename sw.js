c870b94aa47814577910cdf8def6bcd3e435e726
Gerard Tyedmers <gtyedmers@gmx.net>
2021-05-19 19:50:01 +0200

    license-info

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
-/*
-* grrd's 4 in a Row
-* Copyright (c) 2012 Gerard Tyedmers, grrd@gmx.net
-* Licensed under the MPL License
-*/
+/**
+ * grrd's 4 in a Row
+ * Copyright (c) 2012 Gerard Tyedmers, grrd@gmx.net
+ * @license MPL-2.0
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 
 /*jslint devel: true, browser: true */ /*global  self  */
 
